c7afd62f8b5b7d89bd8fd9b0dbdf57bd55ab1bd1
Carlos del Cacho <delcacho@gmail.com>
2024-11-11 13:57:51 +0100

    Add correct version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model_package',
-    version='0.6.1',
+    version='0.6.2',
     packages=find_packages(),
     install_requires=[
         "mlflow==2.15.1", "matplotlib", "scikit-learn", "pycm"
