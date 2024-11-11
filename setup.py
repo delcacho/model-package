8d7699653014fdd4abd050159e4c9c8ade07ec99
Carlos del Cacho <delcacho@gmail.com>
2024-11-11 11:50:49 +0100

    Increase version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='model_package',
-    version='0.2',
+    version='0.3',
     packages=find_packages(),
     install_requires=[
         "mlflow", "matplotlib", "scikit-learn", "pycm"
