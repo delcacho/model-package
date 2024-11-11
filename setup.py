723295b194a0b2c7127cacea71373587db54a133
Carlos del Cacho <delcacho@gmail.com>
2024-11-11 12:47:16 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name='model_package',
-    version='0.6',
+    version='0.6.1',
     packages=find_packages(),
     install_requires=[
-        "mlflow", "matplotlib", "scikit-learn", "pycm"
+        "mlflow==2.15.1", "matplotlib", "scikit-learn", "pycm"
     ],
 )
